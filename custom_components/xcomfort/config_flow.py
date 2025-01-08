29851d4af99e9f5d23a1687ddb8fbe0fe351fcb0
josrothman <jos@connect-smart.nl>
2025-01-08 11:30:38 +0100

    Update config_flow.py

--- custom_components/xcomfort/config_flow.py
+++ custom_components/xcomfort/config_flow.py
@@ -1,52 +1,14 @@
 ###Version 1.3.5
 from homeassistant.const import CONF_NAME
-from homeassistant import config_entries
 import voluptuous as vol
 import logging
+from homeassistant import config_entries
 
-from .const import DOMAIN
-
-_LOGGER = logging.getLogger(__name__)
-
-DATA_SCHEMA = vol.Schema(
-    {
-        vol.Required("url", default="http://10.0.0.20"): str,
-        vol.Required("username", default="admin1"): str,
-        vol.Required("password",default=""): str,
-        vol.Required("zone",default="hz_3"): str,
-        vol.Required("scan_interval", default=5): int,
-        vol.Optional("heating_zone0",default=""): str,
-        vol.Optional("heating_zone0_radiator",default=""): str,
-        vol.Optional("heating_zone1",default=""): str,
-        vol.Optional("heating_zone1_radiator",default=""): str,
-        vol.Optional("heating_zone2",default=""): str,
-        vol.Optional("heating_zone2_radiator",default=""): str,
-        vol.Optional("heating_zone3",default=""): str,
-        vol.Optional("heating_zone3_radiator",default=""): str,
-        vol.Optional("heating_zone4",default=""): str,
-        vol.Optional("heating_zone4_radiator",default=""): str,
-        vol.Optional("heating_zone5",default=""): str,
-        vol.Optional("heating_zone5_radiator",default=""): str
-    }
-)
-
-
-class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    VERSION = 1
+class xComfortConfigFlow(config_entries.ConfigFlow, domain='xcomfort'):
     async def async_step_user(self, user_input=None):
-        _LOGGER.debug("ConfigFlow start")
-        
-        errors = {}
-        
         if user_input is not None:
-            await self.async_set_unique_id(
-                    user_input["url"], raise_on_progress=False
-                )
+            return self.async_create_entry(title="xComfort", data=user_input)
 
-            self._abort_if_unique_id_configured()
-
-            return self.async_create_entry( title="xcomfort",  data=user_input,
-                )
-        return self.async_show_form(
-            step_id="user", data_schema=DATA_SCHEMA, errors=errors,
-        )
+        zones = await self.api.get_zones()
+        schema = vol.Schema({vol.Required("zone", default=zones[0]): vol.In(zones)})
+        return self.async_show_form(step_id="user", data_schema=schema)
